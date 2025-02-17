4a9acbc5d9c07ec6122b649337d0ac1509d06629
Wahit Fitriyanto <26300406+wahitftry@users.noreply.github.com>
2025-02-17 15:25:20 +0000

    Redirect home route to Buku Tamu index view

--- lks/routes/web.php
+++ lks/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BukuTamuController;
 
 Route::get('/', function () {
-    return view('bukutamu.index');
+    return redirect()->route('bukutamu.index');
 });
 
 Route::resource('bukutamu', BukuTamuController::class);

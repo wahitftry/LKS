692e60535eceff8c600f53833c90455292af8eab
Wahit Fitriyanto <26300406+wahitftry@users.noreply.github.com>
2025-02-17 15:22:14 +0000

    Refactor Buku Tamu detail view for improved layout and styling

--- lks/resources/views/bukutamu/show.blade.php
+++ lks/resources/views/bukutamu/show.blade.php
@@ -1,26 +1,27 @@
 @include('layout.header')
 @include('layout.sidebar')
 <div class="container-fluid page-content">
-  <section class="row">
-    <div class="col-12">
-      <div class="card">
-        <div class="card-header">
-          <h3>Detail Buku Tamu</h3>
-        </div>
-        <div class="card-body">
-          <p><strong>Nama:</strong> {{ $bukutamu->nama }}</p>
-          <p><strong>Email:</strong> {{ $bukutamu->email }}</p>
-          <p><strong>Telepon:</strong> {{ $bukutamu->phone }}</p>
-          <p><strong>Kota:</strong> {{ $bukutamu->city }}</p>
-          <p><strong>Status:</strong> {{ $bukutamu->status }}</p>
-          @if($bukutamu->gambar)
-            <div class="mt-3">
-              <p><strong>Gambar:</strong></p>
-              <img src="{{ asset('storage/'.$bukutamu->gambar) }}" width="200">
-            </div>
-          @endif
-          <a href="{{ route('bukutamu.index') }}" class="btn btn-secondary mt-3">Kembali</a>
-        </div>
+  <section class="section">
+    <div class="card">
+      <div class="card-header">
+        <h4 class="card-title">Detail Buku Tamu</h4>
+      </div>
+      <div class="card-body">
+        <!-- Using list group for details -->
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"><strong>Nama:</strong> {{ $bukutamu->nama }}</li>
+          <li class="list-group-item"><strong>Email:</strong> {{ $bukutamu->email }}</li>
+          <li class="list-group-item"><strong>Telepon:</strong> {{ $bukutamu->phone }}</li>
+          <li class="list-group-item"><strong>Kota:</strong> {{ $bukutamu->city }}</li>
+          <li class="list-group-item"><strong>Status:</strong> {{ $bukutamu->status }}</li>
+        </ul>
+        @if($bukutamu->gambar)
+          <div class="mt-3">
+            <p><strong>Gambar:</strong></p>
+            <img src="{{ asset('storage/'.$bukutamu->gambar) }}" class="img-fluid rounded" alt="Gambar">
+          </div>
+        @endif
+        <a href="{{ route('bukutamu.index') }}" class="btn btn-secondary mt-3">Kembali</a>
       </div>
     </div>
   </section>
